8c9281167a8aed114f716b64e7c0ed357b66f574
taylorgeeco-data <>
2026-04-30 10:45:56 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,10 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove("onions")
+vegetables.append("carrots")
+vegetables.append("cucumbers")
+
+vegetables.sort()
+
+print("Updated Vegetable Inventory:", vegetables)
+print("Carrots are already in the list.")
+print("Cucumbers are already in the list.")
