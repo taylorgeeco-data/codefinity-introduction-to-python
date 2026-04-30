fe3cd83fdcb347494a6251ccd550bdf1dee92112
taylorgeeco-data <>
2026-04-30 06:20:41 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,6 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:3]
+dairy2 = grocery_items[6:11]
+bakery1 = grocery_items[13:17]
+
+print(f"We have dairy and bakery items: {dairy1}, {dairy2}, and {bakery1} in aisle 5")
